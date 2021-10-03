865a00e1afbfc5639f01a47502705ade6ade31b7
Erick Alvarez <erick.alvarez@live.com>
2021-10-03 10:14:31 -0700

    next project

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <h1>hello</h1>
+  )
+}
+
+export default Home
